2ecd7cd1ee02b203702d4ce5d505e0b2d4dc7141
Meir Blachman <meirblachman@gmail.com>
2022-04-20 21:16:06 +0300

    expose all implementations

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import { Scenarios as PlaywrightScenarios } from './playwright';
+import { Scenarios as SeleniumScenarios } from './selenium';
+
+export const Scenarios = {
+    playwright: PlaywrightScenarios,
+    selenium: SeleniumScenarios
+};
