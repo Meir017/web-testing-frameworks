51a266cffab22bb2a9e0f9706a5a18bf96decbb4
Meir Blachman <meirblachman@gmail.com>
2022-04-30 22:15:38 +0300

    add chrome options

--- src/utils/selenium.ts
+++ src/utils/selenium.ts
@@ -1,9 +1,13 @@
 import * as seleniumWebdriver from 'selenium-webdriver';
+import * as chrome from 'selenium-webdriver/chrome';
 const { Builder, Browser, By, until } = seleniumWebdriver;
 
 export async function runWithChrome<T>(func: (driver: seleniumWebdriver.WebDriver) => Promise<T>) {
     // await import('chromedriver');
-    const driver = await new Builder().forBrowser(Browser.CHROME).build();
+    const driver = await new Builder()
+        .forBrowser(Browser.CHROME)
+        .setChromeOptions(new chrome.Options().headless())
+        .build();
     try {
         return await func(driver);
     } finally {
