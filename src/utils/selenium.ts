dc1b4a808a33a5214c9fa9c523c53058c516ad88
Meir Blachman <meirblachman@gmail.com>
2022-04-30 22:06:46 +0300

    try without chromedriver

--- src/utils/selenium.ts
+++ src/utils/selenium.ts
@@ -2,7 +2,7 @@ import * as seleniumWebdriver from 'selenium-webdriver';
 const { Builder, Browser, By, until } = seleniumWebdriver;
 
 export async function runWithChrome<T>(func: (driver: seleniumWebdriver.WebDriver) => Promise<T>) {
-    await import('chromedriver');
+    // await import('chromedriver');
     const driver = await new Builder().forBrowser(Browser.CHROME).build();
     try {
         return await func(driver);
